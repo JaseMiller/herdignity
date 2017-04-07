627a83c92158f874877eff91e2c76474f55f1026
JΛSΞ <jase@iteams.org>
2017-04-07 01:19:01 -0500

    Last minute fixes before testing it live

--- single.php
+++ single.php
@@ -3,66 +3,93 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-<div id="stories-banner">
-<div class="image"><?php the_post_thumbnail();?></div>
+	
+	
+		<div id="inner">
+		<div id="resource">
 
-
-<div class="child-title">Issues <span class="diff"><?php
+	
+			<div class="issues-banner">
+				<div class="image"><img class="img-responsive" src="<?php the_post_thumbnail_url();?>" /></div>
+				
+				<div class="child-title">Issues <span class="diff"><?php
 $category = get_the_category(); 
 echo $category[0]->cat_name;
-?></span></div>
-<div class="tagline"><h2>Resource</h2></div>
-</div>
+?></span>
+			</div>
 
-<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
-<!--  End post -->
+			<div class="tagline"><h2>Resource</h2></div>
+			
+			</div>
+			
+			
+		<div class="row banner" style="margin-top: 40px;">
+		<div class="col-md-12 col-sm-12">
 
 
-<div id="inner">
+			
 
-<div id="resource">
-<?php $related_links = get_related_links(true); if ($related_links) {
-?><div class="related-posts"><div class="related-posts"><h2>Additional Resources</h2>
-<?php related_links(); ?></div></div>
+
+			<?php endwhile; else: ?>
+			<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			<?php endif; ?>
+			<!--  End post -->
+
+
+					<?php $related_links = get_related_links(true); if ($related_links) {
+?>
+					<div class="related-posts">
+						<div class="related-posts">
+							<h2>Additional Resources</h2>
+							<?php related_links(); ?>
+						</div>
+					</div>
 <?php } else { ?>
 <?php } ?>
 
-
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<div class="post" id="post-<?php the_ID(); ?>">
-<h3><?php the_title(); ?></h3>
-<div class="meta">
-<div class="left">Post Date: <?php the_date('m/d/Y') ?><br />
-<?php $author = get_post_meta($post->ID, "Author", true);
+					<div class="post" id="post-<?php the_ID(); ?>">
+						<h3><?php the_title(); ?></h3>
+						<div class="meta">
+							<div class="left">Post Date: <?php the_date('m/d/Y') ?><br />
+							<?php $author = get_post_meta($post->ID, "Author", true);
 if ($author != "") { echo 'Author: ' .$author;
 } else {}  ?>
 
-    <?php if( get_field('author') ): ?>
-        <p style="color:#828282; font-weight: 400;"><strong>By:</strong> <?php the_field('author'); ?></p>
-    <?php endif; ?>
-</div>
-<div class="right">Categories: <?php the_category(', '); ?></div>
-</div>
-	<div class="entry">
-		<?php the_content(__('(more...)')); ?>
-	</div>
+    						<?php if( get_field('author') ): ?>
+        					<p style="color:#828282; font-weight: 400;"><strong>By:</strong> <?php the_field('author'); ?></p>
+    						<?php endif; ?>
+							</div><!-- /.left -->
+							<div class="right">Categories: <?php the_category(', '); ?></div>
+						</div>
+						<div class="entry">
+							<?php the_content(__('(more...)')); ?>
+						</div>
+					</div><!--  End post -->
 
-
-</div><!--  End post -->
-<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
-</div><!--  End Inner -->
-<div style="clear:both"> </div>
-<hr class="dotted" />
-<ul class="h-block"><?php query_posts('post_type=featured&showposts=3'); 
-global $more; $more = 0; while (have_posts()) : the_post(); ?>
-<li><a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2>
-<?php the_excerpt(); ?></a></li>
-<?php endwhile; ?>
-<?php wp_reset_query(); ?></ul>
+				</div><!-- /#resource -->
+				
+			</div><!-- /#inner -->
+			
+		</div><!-- /.col -->
+	</div><!-- /.row -->
+			
+<div class="footer-menu row">
+	<div class="col-md-4 col-sm-4 col-xs-12">
+		<a href="http://www.herdignity.net/join/">
+		<h2>Growing the Network</h2><p>Help develop a global network of men &amp; women, individuals &amp; organizations, committed to the broader health needs of girls and women.</p>
+		</a>
+	</div><!-- /.col -->
+	<div class="col-md-4 col-sm-4 col-xs-12">
+		<a href="http://www.herdignity.net/join/">
+		<h2>Reduce Maternal Mortality</h2><p>Join us as we promote continued progress in reducing maternal mortality worldwide through community education, research, and opportunities to make a difference.</p>
+		</a>
+	</div><!-- /.col -->
+	<div class="col-md-4 col-sm-4 col-xs-12">
+		<a href="http://www.herdignity.net/join/">
+		<h2>Network Members</h2><p>Individuals and organizations who have already joined the Network.</p>
+		</a>
+	</div><!-- /.col -->
+</div><!-- /.row -->
 
 </div><!--  End Content -->
 
